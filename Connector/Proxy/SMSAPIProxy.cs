d7a1ca25c5bbefde9a4e0c3e31bdaee8277f03a9
ashwath <ashwath@f9bb96b6-1837-5f47-b166-d5369a8dab9f>
2018-12-03 11:45:28 +0000

    added SMSAPI layer , for monitor hunt traffic command. Currently only one method is used - GetSkills()
    git-svn-id: svn://202.150.209.18/InterLink/Products/DashboardService/TRealtimeData/TRealtimeData@28041 f9bb96b6-1837-5f47-b166-d5369a8dab9f

---
+++ Connector/Proxy/SMSAPIProxy.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Connector.Proxy
+{
+    public class SMSAPIProxy
+    {
+        static SMSAPI.ServiceClient _smsapi = new SMSAPI.ServiceClient();
+
+        public static List<SMSAPI.HuntGroupType> GetSkills()
+        {
+            try
+            {
+                return _smsapi.GetSkillList().ToList();
+            }
+            catch (Exception ex)
+            {
+                
+            }
+            return null;
+        }
+    }
+}
