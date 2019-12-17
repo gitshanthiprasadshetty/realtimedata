d2dd958038c4f97c038af731caeee0cdf10c2090
Shreeraj Bhat <shreeraj.bhat@tetherfi.com>
2019-12-17 11:17:47 +0530

    Added DataContract and DataMember for class properties

--- SIPDataCollector/Models/AgentData.cs
+++ SIPDataCollector/Models/AgentData.cs
@@ -1,17 +1,23 @@
-﻿namespace SIPDataCollector.Models
+﻿using Newtonsoft.Json;
+using System.Runtime.Serialization;
+
+namespace SIPDataCollector.Models
 {
+    [DataContract]
     public class AgentData
     {
         /// <summary>
         /// Avaya Login-Id
         /// </summary>
+        [DataMember(Name = "loginId")]
         public string LoginId { get; set; }
 
         /// <summary>
         /// Agent State-{ACW,Default,Available..}
         /// </summary>
+        [DataMember(Name = "status")]
         public string State { get; set; }
-
+        [DataMember(Name = "stationId")]
         public string StationID { get; set; }
 
     }
--- SIPDataCollector/Models/RealtimeData.cs
+++ SIPDataCollector/Models/RealtimeData.cs
@@ -1,33 +1,62 @@
-﻿using SIPDataCollector.Models;
+﻿using Newtonsoft.Json;
+using SIPDataCollector.Models;
 using System.Collections.Generic;
+using System.Runtime.Serialization;
 
+[DataContract]
 public class RealtimeData
 {
+    [DataMember(Name ="skillId")]
     public int SkillId { get; set; }
+    [DataMember(Name = "skillExtensionId")]
     public int SkillExtensionId { get; set; }
+    [DataMember(Name = "skillName")]
     public string SkillName { get; set; }
+    [DataMember(Name = "interactionsInQueue")]
     public int InteractionsInQueue { get; set; }
+    [DataMember(Name = "activeInteractions")]
     public int ActiveInteractions { get; set; }
+    [DataMember(Name = "activeInteractionsSummary")]
     public int ActiveInteractionsSummary { get; set; }
+    [DataMember(Name = "abandonInteractionsSummary")]
     public int AbandonedInteractionsSummary { get; set; }
+    [DataMember(Name = "oldestInteractionWaitTime")]
     public int OldestInteractionWaitTime { get; set; }
+    [DataMember(Name = "averageAbandonTime")]
     public int AverageAbandonedTime { get; set; }
+    [DataMember(Name = "abandonPercentage")]
     public decimal AbandonPercentage { get; set; }
+    [DataMember(Name = "slPercentage")]
     public decimal SLPercentage { get; set; }
+    [DataMember(Name = "aht")]
     public int AverageHandlingTime { get; set; }
+    [DataMember(Name = "totalAgentStaffed")]
     public int TotalAgentsStaffed { get; set; }
+    [DataMember(Name = "totalAgentAvailable")]
     public int TotalAgentsAvailable { get; set; }
+    [DataMember(Name = "totalAgentACW")]
     public int TotalAgentsInACW { get; set; }
+    [DataMember(Name = "totalAgentAux")]
     public int TotalAgentsInAUX { get; set; }
+    [DataMember(Name = "agentList")]
     public List<AgentData> AgentStats { get; set; }
+    [DataMember(Name = "acceptedSL")]
     public int AcceptedSL { get; set; }
+    [DataMember(Name = "channel")]
     public string Channel { get; set; }
+    [DataMember(Name = "AvgSpeedAnswer")]
     public int AvgSpeedAnswer { get; set; }
+    [DataMember(Name = "activeTime")]
     public int InteractionsActiveTime { get; set; }
+    [DataMember(Name = "holdTime")]
     public int InteractionsHoldTime { get; set; }
+    [DataMember(Name = "InteractionsQueueTime")]
     public int InteractionsQueueTime { get; set; }
+    [DataMember(Name = "TotalInteractionsQueueTime")]
     public int TotalInteractionsQueueTime { get; set; }
+    [DataMember(Name = "acwTime")]
     public int InteractionsAcwTime { get; set; }
+    [DataMember(Name = "callsAnsweredWithinSL")]
     public int CallsAnsweredWithinSLA { get; set; }
 }
 
